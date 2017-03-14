a97456104f5926c2938628da076a248f882752b2
miraclebox-dev <dev@miraclebox.com>
2017-03-14 09:11:33 +0100

    updates for pic

--- lib/gdi/grc.cpp
+++ lib/gdi/grc.cpp
@@ -22,6 +22,7 @@ gRC::gRC(): rp(0), wp(0)
 #else
 ,m_notify_pump(eApp, 1)
 #endif
+,m_spinner_enabled(0), m_spinneronoff(1), m_prev_idle_count(0)
 {
 	ASSERT(!instance);
 	instance=this;
@@ -36,12 +37,10 @@ gRC::gRC(): rp(0), wp(0)
 	int res = pthread_create(&the_thread, &attr, thread_wrapper, this);
 	pthread_attr_destroy(&attr);
 	if (res)
-		eFatal("[gRC] thread couldn't be created");
+		eFatal("RC thread couldn't be created");
 	else
-		eDebug("[gRC] thread created successfully");
+		eDebug("RC thread created successfully");
 #endif
-	m_spinner_enabled = 0;
-	m_spinneronoff = 1;
 }
 
 DEFINE_REF(gRC);
@@ -54,9 +53,9 @@ gRC::~gRC()
 	o.opcode=gOpcode::shutdown;
 	submit(o);
 #ifndef SYNC_PAINT
-	eDebug("[gRC] waiting for gRC thread shutdown");
+	eDebug("waiting for gRC thread shutdown");
 	pthread_join(the_thread, 0);
-	eDebug("[gRC] thread has finished");
+	eDebug("gRC thread has finished");
 #endif
 }
 
@@ -78,7 +77,7 @@ void gRC::submit(const gOpcode &o)
 #else
 			thread();
 #endif
-			//eDebug("[gRC] render buffer full...");
+			//printf("render buffer full...\n");
 			//fflush(stdout);
 			usleep(1000);  // wait 1 msec
 			continue;
@@ -121,7 +120,7 @@ void *gRC::thread()
 #endif
 		if ( rp != wp )
 		{
-				/* make sure the spinner is not displayed when something is painted */
+				/* make sure the spinner is not displayed when we something is painted */
 			disableSpinner();
 
 			gOpcode o(queue[rp++]);
@@ -191,7 +190,7 @@ void *gRC::thread()
 				if (!idle)
 				{
 					if (!m_spinner_enabled)
-						eDebug("[gRC] main thread is non-idle! display spinner!");
+						eDebug("main thread is non-idle! display spinner!");
 					enableSpinner();
 				} else
 					disableSpinner();
@@ -224,7 +223,7 @@ void gRC::enableSpinner()
 {
 	if (!m_spinner_dc)
 	{
-		eDebug("[gRC] enabelSpinner: no spinner DC!");
+		eDebug("no spinner DC!");
 		return;
 	}
 
@@ -246,7 +245,7 @@ void gRC::disableSpinner()
 
 	if (!m_spinner_dc)
 	{
-		eDebug("[gRC] disableSpinner: no spinner DC!");
+		eDebug("no spinner DC!");
 		return;
 	}
 
@@ -646,6 +645,32 @@ void gPainter::sendHide(ePoint point, eSize size)
 }
 
 #ifdef USE_LIBVUGLES2
+void gPainter::sendShowItem(long dir, ePoint point, eSize size)
+{
+	if ( m_dc->islocked() )
+			return;
+	gOpcode o;
+	o.opcode=gOpcode::sendShowItem;
+	o.dc = m_dc.grabRef();
+	o.parm.setShowItemInfo = new gOpcode::para::psetShowItemInfo;
+	o.parm.setShowItemInfo->dir = dir;
+	o.parm.setShowItemInfo->point = point;
+	o.parm.setShowItemInfo->size = size;
+	m_rc->submit(o);
+}
+
+void gPainter::setFlush(bool val)
+{
+	if ( m_dc->islocked() )
+		return;
+	gOpcode o;
+	o.opcode=gOpcode::setFlush;
+	o.dc = m_dc.grabRef();
+	o.parm.setFlush = new gOpcode::para::psetFlush;
+	o.parm.setFlush->enable = val;
+	m_rc->submit(o);
+}
+
 void gPainter::setView(eSize size)
 {
 	if ( m_dc->islocked() )
@@ -875,6 +900,10 @@ void gDC::exec(const gOpcode *o)
 	case gOpcode::sendHide:
 		break;
 #ifdef USE_LIBVUGLES2
+	case gOpcode::sendShowItem:
+		break;
+	case gOpcode::setFlush:
+		break;
 	case gOpcode::setView:
 		break;
 #endif
@@ -888,7 +917,7 @@ void gDC::exec(const gOpcode *o)
 		incrementSpinner();
 		break;
 	default:
-		eFatal("[gDC] illegal opcode %d. expect memory leak!", o->opcode);
+		eFatal("illegal opcode %d. expect memory leak!", o->opcode);
 	}
 }
 
@@ -898,7 +927,7 @@ gRGB gDC::getRGB(gColor col)
 		return gRGB(col, col, col);
 	if (col<0)
 	{
-		eFatal("[gDC] getRGB transp");
+		eFatal("bla transp");
 		return gRGB(0, 0, 0, 0xFF);
 	}
 	return m_pixmap->surface->clut.data[col];
